75b6be84636e819bad3aad30c15a9c4f9dfa5afc
reima <reima.saito@outlook.jp>
2026-02-28 18:27:22 +0900

    refactor: improve theme toggle function structure

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const button = document.getElementById('theme-toggle');
 
+ function updatebuttonText() {
+    if(document.body.classList.contains('dark-mode')) {
+         button.textContent = 'Switch to Light Mode';
+    }else {   
+        button.textContent = 'Switch to Dark Mode';
+    }
+}
+
 button.addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
+    updatebuttonText();
 });
